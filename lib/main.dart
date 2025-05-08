fa163c598f0f99089bbb9079d6df2de765533cfa
Isaac Cortés <icortesmorales21@gmail.com>
2025-05-08 14:56:15 -0500

    3.7. Integración de base de datos 

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hello_flutter/firebase_options.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
